6598f0a23f4bef24b9eafdd2c82a482d248419b4
Byun SoYun <bsy1482@naver.com>
2024-06-30 14:54:40 +0900

    feat: `TipRepository` 생성

---
+++ src/main/kotlin/com/devsy/dieter_community/repository/TipRepository.kt
@@ -0,0 +1,8 @@
+package com.devsy.dieter_community.repository
+
+import com.devsy.dieter_community.domain.Tip
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TipRepository: JpaRepository<Tip, Long>
