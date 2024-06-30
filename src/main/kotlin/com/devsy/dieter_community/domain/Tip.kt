c0a4c55788f43753de5d75f07b546db6483bff39
Byun SoYun <bsy1482@naver.com>
2024-06-30 14:49:54 +0900

    feat: `Tip Entity` 생성

---
+++ src/main/kotlin/com/devsy/dieter_community/domain/Tip.kt
@@ -0,0 +1,35 @@
+package com.devsy.dieter_community.domain
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "tip")
+class Tip(
+    id: Long,
+    title: String,
+    content: String,
+    createdAt: LocalDateTime,
+) {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = id
+
+    @Column(name = "title")
+    var title: String = title
+
+    @Column(name = "content")
+    var content: String = content
+
+    @Column(name = "writer_id")
+    val writerId: Long? = null
+
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime? = null
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime = createdAt
+
+}
