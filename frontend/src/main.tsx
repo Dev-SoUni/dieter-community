5bfd9f1fb26b5a86de329e053e40ab62ed0897a9
Byun SoYun <bsy1482@naver.com>
2024-07-02 17:22:22 +0900

    feat: `MUI` 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import MainPage from './pages/MainPage.tsx'
 import TipPage from './pages/TipPage.tsx'
 import TipNewPage from './pages/TipNewPage.tsx'
