fb780014feb6005ed827b93a2132145c76d6d333
Byun SoYun <bsy1482@naver.com>
2024-07-03 14:34:37 +0900

    feat: `TipResponseDTO`의 LocalDateTime 포맷 설정

--- backend/src/main/kotlin/com/devsy/dieter_community/dto/TipResponseDTO.kt
+++ backend/src/main/kotlin/com/devsy/dieter_community/dto/TipResponseDTO.kt
@@ -1,11 +1,15 @@
 package com.devsy.dieter_community.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
 
 data class TipResponseDTO(
     val id: String,
     val title: String,
     val content: String,
+    @JsonFormat(pattern = "yyy.MM.dd")
     val updatedAt: LocalDateTime,
+    @JsonFormat(pattern = "yyy.MM.dd")
     val createdAt: LocalDateTime,
 )
