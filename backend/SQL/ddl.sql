a037853c09cbb5ae562b7828358f979936d44df8
Byun SoYun <bsy1482@naver.com>
2024-07-05 16:48:54 +0900

    chore: 테이블 스키마 DDL 추가

--- backend/SQL/ddl.sql
+++ backend/SQL/ddl.sql
@@ -1,3 +1,30 @@
+create table member
+(
+    id         varchar(256)            not null
+        constraint member_pk
+            primary key,
+    email      varchar(256)            not null,
+    nickname   varchar(63)             not null,
+    password   varchar(512),
+    updated_at timestamp               not null,
+    created_at timestamp default now() not null
+);
+
+comment on table member is '회원';
+
+comment on column member.email is '이메일';
+
+comment on column member.nickname is '닉네임';
+
+comment on column member.password is '비밀번호';
+
+comment on column member.updated_at is '수정일';
+
+comment on column member.created_at is '등록 날짜';
+
+alter table member
+    owner to dev;
+
 create table tip
 (
     id         varchar(256)            not null
@@ -5,7 +32,9 @@ create table tip
             primary key,
     title      varchar(256)            not null,
     content    text                    not null,
-    writer_id  bigint,
+    writer_id  varchar(256)            not null
+        constraint tip_member_id_fk
+            references member,
     updated_at timestamp               not null,
     created_at timestamp default now() not null
 );
@@ -18,7 +47,7 @@ comment on column tip.title is '꿀팁 게시판 제목';
 
 comment on column tip.content is '꿀팁 내용';
 
-comment on column tip.writer_id is '작성자 id';
+comment on column tip.writer_id is '작성자 아이디';
 
 comment on column tip.updated_at is '수정 날짜';
 
@@ -35,7 +64,9 @@ create table tip_like
     tip_id     varchar(256)
         constraint tip_like_tip_id_fk
             references tip,
-    member_id  varchar(256)            not null,
+    member_id  varchar(256)            not null
+        constraint tip_like_member_id_fk
+            references member,
     created_at timestamp default now() not null
 );
 
