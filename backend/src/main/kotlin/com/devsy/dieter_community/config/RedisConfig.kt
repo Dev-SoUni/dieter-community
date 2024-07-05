36a010bb6c781e2e8f8b1d8a57e62b6d347d92c7
Byun SoYun <bsy1482@naver.com>
2024-07-05 16:14:49 +0900

    feat: `Redis` 설정

---
+++ backend/src/main/kotlin/com/devsy/dieter_community/config/RedisConfig.kt
@@ -0,0 +1,34 @@
+package com.devsy.dieter_community.config
+
+import org.springframework.beans.factory.annotation.Configurable
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configurable
+class RedisConfig(
+    @Value("\${spring.data.redis.host}")
+    val host: String,
+    @Value("\${spring.data.redis.port}")
+    val port: Int,
+) {
+
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*, *> {
+        return RedisTemplate<Any, Any>().apply {
+            this.connectionFactory = redisConnectionFactory()
+
+            // "\xac\xed\x00" 같은 불필요한 해시값을 보지 않기 위해 serializer 설정
+            this.keySerializer = StringRedisSerializer()
+            this.hashKeySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+        }
+    }
+}
