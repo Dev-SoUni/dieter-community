1d0b6ddb68f42c27572bf3edce37c89ee4ef4767
Byun SoYun <bsy1482@naver.com>
2024-07-10 14:16:28 +0900

    test: 꿀팁에 대한 테스트 코드 추가

--- backend/src/test/kotlin/com/devsy/dieter_community/controller/TipControllerTest.kt
+++ backend/src/test/kotlin/com/devsy/dieter_community/controller/TipControllerTest.kt
@@ -1,15 +1,24 @@
 package com.devsy.dieter_community.controller
 
+import com.devsy.dieter_community.domain.Member
+import com.devsy.dieter_community.domain.Tip
+import com.devsy.dieter_community.dto.TipPostRequest
+import com.devsy.dieter_community.repository.MemberRepository
+import com.devsy.dieter_community.repository.TipRepository
+import com.devsy.dieter_community.service.TokenService
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import kotlin.test.Test
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -18,9 +27,64 @@ class TipControllerTest {
     @Autowired
     private lateinit var mock: MockMvc
 
+    @Autowired
+    private lateinit var mapper: ObjectMapper
+
+    @Autowired
+    private lateinit var tokenService: TokenService
+
     @Test
-    @DisplayName("꿀팁 게시물 조회 : 없는 ID로 조회 (실패)")
-    fun 꿀팁_게시물_없는_ID로_조회() {
+    @DisplayName("꿀팁 게시물 전체 조회")
+    fun 꿀팁_게시물_전체_조회() {
+        // Given
+
+        // When
+        val resultActions = mock.perform(
+            MockMvcRequestBuilders
+                .get("/api/tips")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+
+        // Then
+        resultActions
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.number").value(0))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.totalPages").value(1))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.totalElements").value(10))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.content").isArray)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.content.length()").value(10))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    @DisplayName("꿀팁 게시물 ID로 조회 : 존재하는 ID일 경우")
+    fun 꿀팁_게시물_존재하는_ID로_조회() {
+        // Given
+        val tip = tips[0]
+
+        // When
+        val resultActions = mock.perform(
+            MockMvcRequestBuilders
+                .get("/api/tips/${tip.id}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+
+        // Then
+        resultActions
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.id").value(tip.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.title").value(tip.title))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.content").value(tip.content))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.writer.email").value(tip.writer.email))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.writer.nickname").value(tip.writer.nickname))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    @DisplayName("꿀팁 게시물 ID로 조회 : 존재하지 않는 ID일 경우")
+    fun 꿀팁_게시물_존재하지_않는_ID로_조회() {
         // Given
         val id = "wrong_post_id"
 
@@ -38,5 +102,86 @@ class TipControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("\$.status").value(404))
             .andExpect(MockMvcResultMatchers.jsonPath("\$.message").value("해당 꿀팁을 찾을 수 없습니다."))
             .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    @DisplayName("꿀팁 등록 : 로그인하지 않은 상태")
+    fun 꿀팁_등록_미로그인_상태() {
+        // Given
+        val requestBody = TipPostRequest(title = "꿀팁(New)", content = "내용(New)")
+
+        // When
+        val resultActions = mock.perform(
+            MockMvcRequestBuilders
+                .post("/api/tips")
+                .content(mapper.writeValueAsString(requestBody))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+
+        // Then
+        resultActions
+            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.status").value(HttpStatus.UNAUTHORIZED.value()))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.message").value("API 이용에 대한 인증에 실패했습니다."))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    @DisplayName("꿀팁 등록 : 로그인한 상태")
+    fun 꿀팁_등록_로그인_상태() {
+        // Given
+        val accessToken = tokenService.generate(member)
+        val requestBody = TipPostRequest(title = "꿀팁(New)", content = "내용(New)")
+
+        // When
+        val resultActions = mock.perform(
+            MockMvcRequestBuilders
+                .post("/api/tips")
+                .header("Authorization", "Bearer $accessToken")
+                .content(mapper.writeValueAsString(requestBody))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+
+        // Then
+        resultActions
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.id").isNotEmpty)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.title").value(requestBody.title))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.content").value(requestBody.content))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.writer.email").value(member.email))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.writer.nickname").value(member.nickname))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    companion object {
+
+        lateinit var member: Member
+        lateinit var tips: List<Tip>
+
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll(
+            @Autowired memberRepository: MemberRepository,
+            @Autowired tipRepository: TipRepository,
+        ) {
+            member = Member(email = "Lion@example.com", nickname = "사자", password = "1234")
+            memberRepository.save(member)
+
+            tips = listOf(
+                Tip(title = "꿀팁01", writer = member, content = "내용01"),
+                Tip(title = "꿀팁02", writer = member, content = "내용02"),
+                Tip(title = "꿀팁03", writer = member, content = "내용03"),
+                Tip(title = "꿀팁04", writer = member, content = "내용04"),
+                Tip(title = "꿀팁05", writer = member, content = "내용05"),
+                Tip(title = "꿀팁06", writer = member, content = "내용06"),
+                Tip(title = "꿀팁07", writer = member, content = "내용07"),
+                Tip(title = "꿀팁08", writer = member, content = "내용08"),
+                Tip(title = "꿀팁09", writer = member, content = "내용09"),
+                Tip(title = "꿀팁10", writer = member, content = "내용10"),
+            )
+            tipRepository.saveAll(tips)
+        }
     }
 }
