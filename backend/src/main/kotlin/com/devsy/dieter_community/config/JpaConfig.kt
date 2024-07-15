b82d15f851bd23d78edec3eba87a392c57c349ac
Byun SoYun <bsy1482@naver.com>
2024-07-15 15:50:52 +0900

    feat: `SpringConfig`에서 `EnableJpaAuditing` 관련 설정 클래스 분리

--- backend/src/main/kotlin/com/devsy/dieter_community/config/JpaConfig.kt
+++ backend/src/main/kotlin/com/devsy/dieter_community/config/JpaConfig.kt
@@ -1,6 +1,8 @@
 package com.devsy.dieter_community.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @Configuration
-class SpringConfig
+@EnableJpaAuditing
+class JpaConfig
