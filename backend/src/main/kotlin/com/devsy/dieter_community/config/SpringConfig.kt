da7b606fe362b32cac23d8be4584e62d4f15ce8d
Byun SoYun <bsy1482@naver.com>
2024-07-17 14:53:19 +0900

    feat: JWT 관련 환경 변수를 별도의 Yaml 파일로 분리

--- backend/src/main/kotlin/com/devsy/dieter_community/config/SpringConfig.kt
+++ backend/src/main/kotlin/com/devsy/dieter_community/config/SpringConfig.kt
@@ -1,6 +1,8 @@
 package com.devsy.dieter_community.config
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableConfigurationProperties(JwtProperties::class)
 class SpringConfig
