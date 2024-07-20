08823e2971b32514bc0a0faf9d3e58c382a62245
Byun SoYun <bsy1482@naver.com>
2024-07-20 16:42:28 +0900

    refactor: `Error Response` 설정 부분을 함수화

--- backend/src/main/kotlin/com/devsy/dieter_community/filter/JwtExceptionFilter.kt
+++ backend/src/main/kotlin/com/devsy/dieter_community/filter/JwtExceptionFilter.kt
@@ -1,14 +1,13 @@
 package com.devsy.dieter_community.filter
 
 import com.devsy.dieter_community.dto.ErrorResponse
+import com.devsy.dieter_community.exception.ErrorCode
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.JwtException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
@@ -16,7 +15,6 @@ import org.springframework.web.filter.OncePerRequestFilter
 @Component
 class JwtExceptionFilter : OncePerRequestFilter() {
 
-    private val log = LoggerFactory.getLogger(this.javaClass)
     private val mapper = ObjectMapper()
 
     override fun doFilterInternal(
@@ -28,32 +26,26 @@ class JwtExceptionFilter : OncePerRequestFilter() {
             filterChain.doFilter(request, response)
         } catch (ex: JwtException) {
             when (ex) {
-                is ExpiredJwtException -> {
-                    response.status = HttpStatus.UNAUTHORIZED.value()
-                    response.contentType = MediaType.APPLICATION_JSON_VALUE
-                    response.characterEncoding = "UTF-8"
-                    response.writer.write(mapper.writeValueAsString(
-                        ErrorResponse(
-                            status = HttpStatus.UNAUTHORIZED.value(),
-                            message = "토큰이 만료되었습니다."
-                        )
-                    ))
-                }
-                else -> {
-                    response.status = HttpStatus.UNAUTHORIZED.value()
-                    response.contentType = MediaType.APPLICATION_JSON_VALUE
-                    response.characterEncoding = "UTF-8"
-                    response.writer.write(mapper.writeValueAsString(
-                        ErrorResponse(
-                            status = HttpStatus.UNAUTHORIZED.value(),
-                            message = "토큰을 처리하는 중 문제가 발생했습니다."
-                        )
-                    ))
-                }
+                is ExpiredJwtException ->
+                    setErrorResponse(response, ErrorCode.TOKEN_EXPIRED)
+                else ->
+                    setErrorResponse(response, ErrorCode.TOKEN_ERROR)
             }
         }
     }
 
-    private fun setErrorResponse(response: HttpServletResponse) {
+    private fun setErrorResponse(
+        response: HttpServletResponse,
+        code: ErrorCode,
+    ) {
+        response.status = code.status.value()
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+        response.writer.write(mapper.writeValueAsString(
+            ErrorResponse(
+                status = code.status.value(),
+                message = code.message
+            )
+        ))
     }
 }
