dc2c7bc049b6e0c86a4f967b467346972c58c531
Byun SoYun <bsy1482@naver.com>
2024-07-22 22:07:08 +0900

    test: `logout` API에 대한 통합 테스트 코드 작성

--- backend/src/test/kotlin/com/devsy/dieter_community/controller/AuthControllerTest.kt
+++ backend/src/test/kotlin/com/devsy/dieter_community/controller/AuthControllerTest.kt
@@ -2,19 +2,23 @@ package com.devsy.dieter_community.controller
 
 import com.devsy.dieter_community.domain.Member
 import com.devsy.dieter_community.dto.AuthenticationRequest
+import com.devsy.dieter_community.repository.RefreshTokenRepository
+import com.devsy.dieter_community.service.AuthenticationService
 import com.devsy.dieter_community.service.MemberService
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.transaction.Transactional
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.CookieResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import kotlin.test.Test
@@ -31,7 +35,13 @@ class AuthControllerTest {
     private lateinit var mapper: ObjectMapper
 
     @Autowired
+    private lateinit var authenticationService: AuthenticationService
+
+    @Autowired
     private lateinit var memberService: MemberService
+
+    @Autowired
+    private lateinit var refreshTokenRepository: RefreshTokenRepository
 
     @BeforeEach
     fun beforeEach() {
@@ -106,6 +116,69 @@ class AuthControllerTest {
                     .andExpect(MockMvcResultMatchers.status().isBadRequest)
                     .andExpect(MockMvcResultMatchers.jsonPath("\$.status").value(400))
                     .andExpect(MockMvcResultMatchers.jsonPath("\$.message").value("이메일 또는 비밀번호가 일치하지 않습니다."))
+                    .andDo(MockMvcResultHandlers.print())
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("로그아웃")
+    inner class Logout {
+
+        @Nested
+        @DisplayName("성공 케이스")
+        inner class SuccessCase {
+
+            @Test
+            @DisplayName("logout() : 성공")
+            fun logout() {
+                // Given
+                val authentication = authenticationService.authentication(
+                    email = "Lion@example.com",
+                    password = "12345678"
+                )
+
+                // When
+                val resultActions = mock.perform(
+                    MockMvcRequestBuilders.post("/api/auth/logout")
+                        .header("Authorization", "Bearer ${authentication.accessToken}")
+                        .cookie(authentication.refreshTokenCookie)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+
+                // Then
+                resultActions
+                    .andExpect(MockMvcResultMatchers.status().isNoContent())
+                    .andExpect(MockMvcResultMatchers.cookie().maxAge("refreshToken", 0))
+                    .andDo(MockMvcResultHandlers.print())
+
+                val redisToken = refreshTokenRepository.findByIdOrNull("Lion@example.com")
+                assertThat(redisToken).isNull()
+            }
+        }
+
+        @Nested
+        @DisplayName("실패 케이스")
+        inner class FailCase {
+
+            @Test
+            @DisplayName("logout() : 미로그인")
+            fun logout_미로그인() {
+                // Given
+
+                // When
+                val resultActions = mock.perform(
+                    MockMvcRequestBuilders.post("/api/auth/logout")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+
+                // Then
+                resultActions
+                    .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+                    .andExpect(MockMvcResultMatchers.jsonPath("\$.code").value(HttpStatus.UNAUTHORIZED.name))
+                    .andExpect(MockMvcResultMatchers.jsonPath("\$.status").value(HttpStatus.UNAUTHORIZED.value()))
                     .andDo(MockMvcResultHandlers.print())
             }
         }
