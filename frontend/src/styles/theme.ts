d6aa014d49d9628739d58c7c6a479c1c0d6b6a65
Byun SoYun <bsy1482@naver.com>
2024-07-26 15:50:47 +0900

    style(FE): `primary` 색상 변경

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,7 +1,11 @@
 import { createTheme } from '@mui/material'
-import { orange, red } from '@mui/material/colors'
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#94a3b8',
+    },
+  },
   components: {
     MuiButton: {
       styleOverrides: {
@@ -11,71 +15,12 @@ export const theme = createTheme({
           borderRadius: '12px',
           boxShadow: 'none',
           transition: 'none',
-
-          '&.MuiButton-contained.MuiButton-colorPrimary': {
-            color: '#4e5968',
-            backgroundColor: '#f2f4f6',
-
-            '&:hover': {
-              backgroundColor: '#e5e8eb',
-              boxShadow: 'none',
-            },
-          },
-        },
-      },
-    },
-    MuiInput: {
-      styleOverrides: {
-        root: {
-          '&::before, &::after': {
-            borderBottom: `2px solid #E0E3E7`,
-          },
-
-          '&.MuiInputBase-colorPrimary': {
-            '&:hover:not(.Mui-disabled, .Mui-error):before': {
-              borderBottom: '2px solid #B2BAC2',
-            },
-
-            '&.Mui-focused:after': {
-              borderBottom: '2px solid #6F7E8C',
-            },
-          },
-          '&.MuiInputBase-colorWarning': {
-            '&:hover:not(.Mui-disabled, .Mui-error):before': {
-              borderBottom: `2px solid ${orange['200']}`,
-            },
-
-            '&.Mui-focused:after': {
-              borderBottom: `2px solid ${orange['500']}`,
-            },
-          },
-          '&.MuiInputBase-colorError': {
-            '&:hover:not(.Mui-disabled, .Mui-error):before': {
-              borderBottom: `2px solid ${red['200']}`,
-            },
-
-            '&.Mui-focused:after': {
-              borderBottom: `2px solid ${red['500']}`,
-            },
-          },
         },
       },
     },
     MuiFormLabel: {
       styleOverrides: {
-        root: {
-          '&.Mui-focused': {
-            '&.MuiFormLabel-colorPrimary': {
-              color: '#000',
-            },
-            '&.MuiFormLabel-colorWarning': {
-              color: orange['500'],
-            },
-            '&.MuiFormLabel-colorError': {
-              color: red['500'],
-            },
-          },
-        },
+        // root:
         asterisk: {
           display: 'none',
         },
